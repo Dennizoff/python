659fdfd869ae4b6d9576201b3b6eeaa0bf1c82c4
Dennizoff <imaxsanya@gmail.com>
2023-02-17 18:47:16 +0100

    fff

--- game/game.py
+++ game/game.py
@@ -1,8 +1,14 @@
 import pgzrun
 import pygame.mouse
+import random
 
 WIDTH = 500
 HEIGHT = 400
+
+class Bonus():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
 
 class Ball():
     def __init__(self, x, y, r):
@@ -24,11 +30,21 @@ class Block():
         self.w = w
         self.h = h
 
+class Extralife():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+extralifes = []
+
+
 ball = Ball(WIDTH/2, HEIGHT-100, 10)
 
-paddle = Paddle(WIDTH/2-50, HEIGHT-100, 100, 20)
+paddle = Paddle(WIDTH/2-50, HEIGHT-20, 100, 20)
 
 blocks = []
+
 
 for i in range(7):
     blocks.append(Block(10 + 70*i, 30, 60, 20))
@@ -41,38 +57,87 @@ speed = 5
 speedX = speed
 speedY = speed
 
+lifes = 3
+score = 0
+
 pygame.mouse.set_visible(False)
 
 background = pygame.image.load('./img/1.jpg')
+heart = pygame.image.load('./img/heart.png')
+heart = pygame.transform.scale(heart, (20,20))
+extralife_img = pygame.image.load('./img/heart.png')
+extralife_img = pygame.transform.scale(extralife_img, (20,20))
+bonus_img = pygame.image.load('./img/heart.png')
 
 def draw():
     screen.blit(background,(0, 0))
+
 
     screen.draw.filled_circle((ball.x,ball.y), ball.r, 'red')
     screen.draw.filled_rect(Rect((paddle.x,paddle.y), (paddle.w, paddle.h)), 'green')
 
     for block in blocks:
         screen.draw.filled_rect(Rect((block.x,block.y), (block.w, block.h)), 'orange')
+    for life in range(lifes):
+        screen.blit(heart,(30*life, 0))
 
-    screen.draw.text("Score: "+str(ball.x), (100,0), color="black")
+    for extralife in extralifes:
+        screen.blit(extralife_img,(extralife.x, extralife.y))
+
+    # screen.draw.text("Lifes: "+str(lifes), (10,0), color="black")
+    screen.draw.text("Score: "+str(score), (WIDTH-100,0), color="black")
+    if lifes == 0:
+        screen.draw.text("Game Over", (WIDTH/2-100,HEIGHT/2-24), color="black", fontsize=48)
+    if len(blocks) == 0:
+        screen.draw.text("You Win!", (WIDTH/2-100,HEIGHT/2-24), color="black", fontsize=48)
 
 def update():
-    global speedX, speedY
+    global speedX, speedY, score, lifes
 
     paddle.x = pygame.mouse.get_pos()[0]
-    
+
     if ball.x >= WIDTH or ball.x < 0:
         speedX *= -1
     if ball.y >= HEIGHT or ball.y < 0:
         speedY *= -1
+    if ball.y >= HEIGHT:
+        lifes-=1
     ball.x+=speedX
     ball.y+=speedY
 
+    for extralife in extralifes:
+        extralife.y += 1
+        if extralife.y > HEIGHT:
+            extralifes.remove(extralife)
+        elif  paddle.y <= extralife.y+20 <= paddle.y+paddle.h and paddle.x <= extralife.x+10 <= paddle.x+paddle.w:
+            extralifes.remove(extralife)
+            lifes += 1
+        
+
+
     if paddle.y <= ball.y <= paddle.y+paddle.h and paddle.x <= ball.x <= paddle.x+paddle.w:
         speedY *= -1  
-        
-    if blocks.Block.x <= ball.y <= blocks.Block.y+blocks.Block.h and blocks.Block.x <= ball.x <= blocks.Block.x+blocks.Block.w:
-        speedY *= -1  and blocks.pop(Block)
+
+    for block in blocks:
+        if block.y <= ball.y <= block.y+block.h and block.x <= ball.x <= block.x+block.w:
+            speedY *= -1
+            blocks.remove(block)
+            score+=1
+            rand = random.randrange(0, 2)
+            print(rand)
+            if rand == 0:
+                extralifes.append(Extralife(block.x, block.y))
+    
+
+
+    if lifes == 0 or len(blocks) == 0:
+        speedX = 0
+        speedY = 0
+        ball.x = WIDTH/2
+        ball.y = HEIGHT-100
+
 
 pgzrun.go()
-# dfgrgrgr
+# next step
+# серце, появляються рандомно коли наш мяч знищує блок, на його місці може появитися серце, який буде падати, добавити додаткове життя
+# бонуси, появляються рандомно коли наш мяч знищує блок, на його місці може появитися бонус, який буде падати, робити платформу 2х
